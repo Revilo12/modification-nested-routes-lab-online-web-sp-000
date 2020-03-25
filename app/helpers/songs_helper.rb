10ae7a2d76280b9cfe05dc752cc3e9b7c47bce35
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-25 19:03:00 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,8 +1,8 @@
 module SongsHelper
-  artist_id_field(song)
+  def artist_id_field(song)
   if song.artist.nil?
     select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
   else
-    hidden_field_tag "post[author_id]", post.author_id
+    hidden_field_tag "song[author_id]", post.author_id
   end
 end
